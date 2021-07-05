976be74d35419d8d7cb09010a758db20f0eb2061
sevdas <sshukrieva@gmail.com>
2021-07-05 11:30:21 +0100

    keep track of query and movies data

--- src/components/app/SearchMovies.js
+++ src/components/app/SearchMovies.js
@@ -1,8 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function SearchMovies() {
+  //Input query
+  const [query, setQuery] = useState("");
+  //Update movie state
+  const [movies, setMovies] = useState([]);
+
+  const handleEvent = (event) => {
+    const { value } = event.target;
+    setQuery(value);
+  };
+
+  const searchMovies = async (event) => {
+    event.preventDefault();
+    console.log("submitting");
+
+    const REACT_APP_MOVIE_API_KEY = process.env.REACT_APP_MOVIE_API_KEY;
+
+    const url = `https://api.themoviedb.org/3/search/movie/?api_key=${REACT_APP_MOVIE_API_KEY}&language=en-US&query=${query}&page=1&include_adult=false`;
+
+    try {
+      const response = await fetch(url);
+      const data = await response.json();
+      // console.log(data.results);
+      setMovies(data.results);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  // console.log("movies", movies);
+
   return (
-    <form className="form">
+    <form className="form" onSubmit={searchMovies}>
       <label className="label" htmlFor="query">
         Movie Name
       </label>
@@ -11,6 +41,8 @@ export default function SearchMovies() {
         type="text"
         name="query"
         placeholder="i.e. Jurassic Park"
+        value={query}
+        onChange={handleEvent}
       />
       <button className="button" type="submit">
         Search
